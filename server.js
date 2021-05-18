42c61c3e91af1d2d743d092db02c2f1172e41dc3
kevin hidalgo <kevinrhidalgo@yahoo.com>
2021-05-18 18:53:03 -0400

    trying to deploy

--- server.js
+++ server.js
@@ -15,15 +15,12 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static("public"));
-mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://localhost/budgettrackpwa',
-  {
+mongoose.connect(process.env.MONGODB_URI || `mongodb://localhost/budgettrackpwa`, {
     useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
     useFindAndModify: false,
-  },
-);
+    useUnifiedTopology: true
+});
+
 
 
 // routes
