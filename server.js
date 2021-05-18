35ae90a890b7da18295d8ed1f16fe33bebcf1d41
kevin hidalgo <kevinrhidalgo@yahoo.com>
2021-05-17 23:15:54 -0400

    trying to deploy

--- server.js
+++ server.js
@@ -14,14 +14,8 @@ app.use(compression());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use(express.static("public")); 
+app.use(express.static("public"));
 
-const mongoUri = process.env.MONGODB_URI || "mongodb://localhost/budget";
-mongoose.connect(mongoUri, {
-  useNewUrlParser: true,
-  useFindAndModify: false,
-  useUnifiedTopology: true
-});
 
 // routes
 app.use(require("./routes/api.js"));
